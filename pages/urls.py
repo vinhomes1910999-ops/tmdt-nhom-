c80d8d36080cec5e13ecd461e195f8ead8771a02
buitathiennhan1-sudo <buitathiennhan1@gmail.com>
2026-08-28 18:30:05 +0700

    Add backend API URLs

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'pages'
 
 urlpatterns = [
-    # TV sẽ thêm các path vào đây
+    path('api/pages/', views.page_list, name='page-list'),
+    path('api/pages/<slug:slug>/', views.page_detail, name='page-detail'),
+    path('api/contact/', views.contact_message, name='contact-message'),
 ]
