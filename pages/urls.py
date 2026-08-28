b04c58c6c0f193caf0735b8969fc18922c4160e0
buitathiennhan1-sudo <buitathiennhan1@gmail.com>
2026-08-28 18:52:17 +0700

    Add policy and Google Maps API

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,16 @@ urlpatterns = [
     path('api/pages/', views.page_list, name='page-list'),
     path('api/pages/<slug:slug>/', views.page_detail, name='page-detail'),
     path('api/contact/', views.contact_message, name='contact-message'),
+
+    path(
+        'api/policy/',
+        views.policy,
+        name='policy'
+    ),
+
+    path(
+        'api/location/',
+        views.store_location,
+        name='store-location'
+    ),
 ]
