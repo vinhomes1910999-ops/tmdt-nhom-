290d64e02c3338ee071026a2dd77f04d40f05e13
buitathiennhan1-sudo <buitathiennhan1@gmail.com>
2026-08-28 18:42:53 +0700

    Add review and comment URLs

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from . import views
 
 app_name = 'products'
 
 urlpatterns = [
-    # TV sẽ thêm các path vào đây
+    # Review sản phẩm
+    path(
+        '<int:product_id>/review/',
+        views.add_review,
+        name='add_review'
+    ),
+
+    # Comment sản phẩm
+    path(
+        '<int:product_id>/comment/',
+        views.add_comment,
+        name='add_comment'
+    ),
 ]
